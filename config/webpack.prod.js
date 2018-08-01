aafac7a4fc10c88119a79f4e1d1b47e3d89e0ec0
Alex <alexofrontend@gmail.com>
2018-07-31 22:35:30 -0400

    no message

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -11,29 +11,18 @@ module.exports = merge(commonConfig, {
     filename: 'js/[name].[hash].js',
     chunkFilename: '[id].[hash].chunk.js'
   },
-  optimization: {
-      minimize: true,
-      runtimeChunk: true,
-      splitChunks: {
-          chunks: "async",
-          minSize: 1000,
-          minChunks: 2,
-          maxAsyncRequests: 5,
-          maxInitialRequests: 3,
-          name: true,
-          cacheGroups: {
-              default: {
-                  minChunks: 1,
-                  priority: -20,
-                  reuseExistingChunk: true,
-              },
-              vendors: {
-                  test: /[\\/]node_modules[\\/]/,
-                  priority: -10
-              }
-          }
-      }
-  }
+  {
+    devtool: 'source-map',
+    optimization: {
+      minimizer: [
+        new UglifyJsPlugin({
+          cache: true,
+          sourceMap: true,
+          parallel: true,
+        }),
+      ],
+    },
+  },
   // plugins: [
   //   new webpack.optimize.UglifyJsPlugin({
   //     compressor: {
