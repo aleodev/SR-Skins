01c3f6bc7591b6222a3f36bd0236c401a18dd278
Alex <alexofrontend@gmail.com>
2018-08-04 17:22:25 -0400

    File Changes

--- server/server.js
+++ server/server.js
@@ -77,11 +77,11 @@ var server = app.listen(port, (err) => {
   }
 })
 //////////////////
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*")
-//   res.header("Access-Control-Allow-Headers", "Origin, Content-Type")
-//   next()
-// })
+app.use(function(req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Content-Type")
+  next()
+})
 //////////////////
 const io = socketio(server)
 //////////////////
