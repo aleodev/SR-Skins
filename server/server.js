b2bec8277ad14ca107ecd69a3c1e8671d440a836
Alex <alexofrontend@gmail.com>
2018-08-03 22:24:51 -0400

    File Changes

--- server/server.js
+++ server/server.js
@@ -78,7 +78,7 @@ var server = app.listen(port, (err) => {
 //////////////////
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
-//   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT")
+  res.header("Access-Control-Allow-Methods", "POST")
 //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
   next()
 })
