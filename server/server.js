8bc73f9575a2ade0da773d445741a8a6c088bee8
Alex <alexofrontend@gmail.com>
2018-08-03 23:38:47 -0400

    Production Build

--- server/server.js
+++ server/server.js
@@ -78,7 +78,6 @@ var server = app.listen(port, (err) => {
 //////////////////
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
-  // res.header("Access-Control-Allow-Methods", "POST")
   res.header("Access-Control-Allow-Headers", "Origin, Content-Type")
   next()
 })
