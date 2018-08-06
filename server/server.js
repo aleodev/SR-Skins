ccafc476de27a03d13d91aadcbf3cf9dafe32c5d
Alex <alexofrontend@gmail.com>
2018-08-05 23:56:09 -0400

    File Changes

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ const app = express()
 app.use(helmet())
 app.disable('x-powered-by')
 app.use(function(req, res, next){
-  res.header('Access-Control-Allow-Origin', 'http://70.32.30.254');
+  res.header('Access-Control-Allow-Origin', 'http://70.32.30.252');
   res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   next()
