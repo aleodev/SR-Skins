bf0933deef92ea5aa6566249b602079e249a2cd8
Alex <alexofrontend@gmail.com>
2018-08-05 23:55:35 -0400

    Production Build

--- server/server.js
+++ server/server.js
@@ -34,8 +34,8 @@ const app = express()
 app.use(helmet())
 app.disable('x-powered-by')
 app.use(function(req, res, next){
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Origin', 'http://70.32.30.254');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   next()
 })
