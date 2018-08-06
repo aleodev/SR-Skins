fd9642e494cdae107cc6d55105ad2c160c73a758
Alex <alexofrontend@gmail.com>
2018-08-05 21:40:05 -0400

    File Changes

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ const counters = require('./routes/api/counters')
 app.use(function(req, res, next){
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   next()
 })
 app.use('/', counters)
