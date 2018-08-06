fc8dfdc562ab39db00b40dc74d55ed64d2b1aace
Alex <alexofrontend@gmail.com>
2018-08-05 21:45:19 -0400

    File Changes

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ app.use(express.json({limit: '100mb'}))
 const skineditor = require('./routes/skineditor/spritesheet')
 const counters = require('./routes/api/counters')
 app.use(function(req, res, next){
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'http://70.32.30.254');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   next()
