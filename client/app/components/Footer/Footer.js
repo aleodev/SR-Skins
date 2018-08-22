4e992391b9fbb631d25fb7b7f9b62d88e4067246
Alex <alexofrontend@gmail.com>
2018-08-21 22:53:40 -0400

    Production Build

--- client/app/components/Footer/Footer.js
+++ client/app/components/Footer/Footer.js
@@ -4,6 +4,11 @@ const Footer = () => (
   <section id="footer">
     Created by <a href="https://github.com/aleodev">Slevero//Syrian Limabean</a>{" "}
     & <a href="https://github.com/pop4959">pop4959</a>
+    <br />
+    SpeedRunners is developed by{" "}
+    <a href="http://www.doubledutchgames.com/">DoubleDutch Games</a> and
+    Co-developed and published by{" "}
+    <a href="http://tinybuild.com/speedrunners">tinyBuild GAMES</a>
   </section>
 );
 
