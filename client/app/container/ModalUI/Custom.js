05e0d85cb379d1b511a0e53f8564d6c417d3faf4
Alex <alexofrontend@gmail.com>
2018-08-28 17:05:40 -0400

    Production Build

--- client/app/container/ModalUI/Custom.js
+++ client/app/container/ModalUI/Custom.js
@@ -55,9 +55,7 @@ class Custom extends Component {
     this.setState({ buttonState: "loading" });
     axios({
       method: "POST",
-      url: `https://${process.env.IP_ENV}:${
-        process.env.PORT_ENV
-      }/editor/custom`,
+      url: `http://${process.env.IP_ENV}:${process.env.PORT_ENV}/editor/custom`,
       data: {
         frame_data: this.props.frameData,
         options: this.state.options
