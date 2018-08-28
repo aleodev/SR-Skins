b563baec1e1f593f19809f26309d88ece364ba1f
Alex <alexofrontend@gmail.com>
2018-08-28 02:28:08 -0400

    Production Build

--- server/routes/skineditor/spritesheet.js
+++ server/routes/skineditor/spritesheet.js
@@ -13,7 +13,7 @@ module.exports = app => {
   connections = [];
   //////////////////
   // skin editor post request
-  app.post("/skineditor", function(req, res) {
+  app.post("/editor/custom", function(req, res) {
     if (connections.includes(req.connection.remoteAddress) !== true) {
       connections.push(req.connection.remoteAddress);
       IP_ADD = req.connection.remoteAddress;
