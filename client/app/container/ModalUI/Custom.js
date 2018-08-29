42833ddcb1492b5f79bac455df87146d0db7339a
Alex <alexofrontend@gmail.com>
2018-08-29 00:54:43 -0400

    Production Build

--- client/app/container/ModalUI/Custom.js
+++ client/app/container/ModalUI/Custom.js
@@ -55,7 +55,7 @@ class Custom extends Component {
     this.setState({ buttonState: "loading" });
     axios({
       method: "POST",
-      url: `https://srskins.com/api`,
+      url: `${process.env.IP_ENV}`,
       data: {
         frame_data: this.props.frameData,
         options: this.state.options
