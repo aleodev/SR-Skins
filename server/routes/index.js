26a99e8dbab6d591304869f9ea36b1c447692e2d
Alex <alexofrontend@gmail.com>
2018-09-08 15:43:08 -0400

    File Changes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,14 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-module.exports = (app) => {
+module.exports = app => {
   // API routes
-  fs.readdirSync(__dirname + '/api/').forEach((file) => {
-    require(`./api/${file.substr(0, file.indexOf('.'))}`)(app);
-  });
-  fs.readdirSync(__dirname + '/skineditor/').forEach((file) => {
-    require(`./skineditor/${file.substr(0, file.indexOf('.'))}`)(app);
-  });
+  // fs.readdirSync(__dirname + "/api/").forEach(file => {
+  //   require(`./api/${file.substr(0, file.indexOf("."))}`)(app);
+  // });
+  // fs.readdirSync(__dirname + "/skineditor/").forEach(file => {
+  //   require(`./skineditor/${file.substr(0, file.indexOf("."))}`)(app);
+  // });
+  require(`./api/counters.js`)(app);
+  require(`./skineditor/spritesheet.js`)(app);
 };
